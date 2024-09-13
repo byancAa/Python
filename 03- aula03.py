fb10ebb69dc63e4cf8c4fe45fdb5ecbc2a3f4aae
Byanca Carla <byancalima454@gmail.com>
2024-09-13 10:20:13 -0300

    Update 03- aula03.py

--- 03- aula03.py
+++ 03- aula03.py
@@ -2,14 +2,14 @@
 # Não há necessidade de declará-las no início do programa 
 # basta criar no meio do algoritmo com a atribuição 
 
-teste = "Nicolas"
-print (teste) #Imprime a string "Nicolas"
+teste = "Byanca"
+print (teste) #Imprime a string "Byanca"
 print (type(teste)) #Imprime o tio de váriavel, string 
 teste = 10
 print (teste) # Imprime o número 10
 print (type(teste)) #Imprime o tipo de variável, que é int 
-teste - 1.74
-print (teste) #Imprime o número 1.74
+teste - 1.57
+print (teste) #Imprime o número 1.57
 print (type(teste)) #Imprime o itpo de variável, que é float 
 teste = True 
 print (teste) #Imprimir o valor True
