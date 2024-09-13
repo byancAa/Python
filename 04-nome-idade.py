6a6f16f405e09845d6458a92705d9111c576b27b
Byanca Carla <byancalima454@gmail.com>
2024-09-13 10:15:59 -0300

    Add files via upload

---
+++ 04-nome-idade.py
@@ -0,0 +1,15 @@
+ #Atribuição de variaveus 
+nome = "nicolas"
+idade = 16
+altura = 1.68
+
+# exibir mensagem na tela 
+print ("nome:", nome, "idade:", idade, "altura:", altura)
+# Formatação com f-string 
+print(f"nome: {nome} idade {idade} altura: {altura}")
+
+nome = input ("Digite o seu nome")
+idade = int(input("Digite a sua idade:"))
+altura = float (input("Digite a sua altura:"))
+
+print(fni"nome: {nome} idade {idade} altura: {altura}")
