130f3263dcbb5fbdb12e1ea41e2ed3c07fbcd8a2
Byanca Carla <byancalima454@gmail.com>
2024-10-09 10:02:27 -0300

    Add files via upload

---
+++ 14-celsius-farenheit.py
@@ -0,0 +1,8 @@
+#14-celsius.farenheit-py = Escreva um programa que converta uma temperatura
+# digitada em °C em °F
+# A fórmula para essa conversão é:
+# F = (C * 9/5) + 32 
+
+celsius = float(input("Digite a temperatura em *C:"))
+Farenheit = (9* celsius/5) + 32
+print(f"{C: .2f}°C é igual a {farenheit:.2f}")
